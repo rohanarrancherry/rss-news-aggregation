1a3f098c5711cb71a989bc6820251a82b39595ae
rohanarrancherry <rohanarrancherry@gmail.com>
2022-03-21 11:02:50 +0000

    moved to main branch

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'; 
+import LoginUi from './loginUI/login';
+
+ReactDOM.render( 
+    <LoginUi /> ,
+  document.getElementById('root')
+);
+ 
