b30288228f4de09c2e7a9418fba75e4521560704
sanleypeter96 <sanleypeter96@gmail.com>
2022-04-18 00:29:11 +0100

    Refactor code in category menu

--- client/src/components/CategoryMenu/CategoryMenu.js
+++ client/src/components/CategoryMenu/CategoryMenu.js
@@ -1,24 +1,10 @@
 import React, {useState, useEffect} from 'react';
-// import PropTypes from 'prop-types';
-import {Tabs, Tab} from '@material-ui/core';
-import {makeStyles} from "@material-ui/core/styles";
 import { Navbar, Nav } from 'react-bootstrap';
-import { Row, Container } from "reactstrap";
-import axios from 'axios';
-import {useNavigate} from "react-router-dom";
-const useTabStyles = makeStyles({
-    root: {
-        justifyContent: "center"
-    },
-    scroller: {
-        flexGrow: "0"
-    }
-});
+import { Container } from "reactstrap";
 
 const CategoryMenu = () => {
     const [active, setActive] = useState("latest");
     const [cat, setCat] = useState([]);
-    let navigate = useNavigate()
 
     function getCategories() {
         const url = "/api/user/categories";
@@ -35,7 +21,6 @@ const CategoryMenu = () => {
         getCategories()
     }, [])
 
-    const classes = useTabStyles();
 
     return (
             
@@ -49,25 +34,6 @@ const CategoryMenu = () => {
                 </Container>
             </Navbar>
             </> 
-
-        // <Tabs
-        //     classes={{root: classes.root, scroller: classes.scroller}}
-        //     value={active}
-        //     onChange={(event, newValue) => {
-        //         console.log(newValue);
-        //         setActive(newValue);
-                
-        //     }}
-        //     indicatorColor="primary"
-        //     textColor="primary"
-        //     variant={"scrollable"}
-        //     scrollButtons={"on"}
-        // >
-        //     {cat.map((category, index) => (
-        //         <Tab key={index}  onClick={()=>{navigate(`/dashboard/${category}`)}} label={category} value={category}/>
-        //     ))}
-        // </Tabs>
-
         
     );
 };
