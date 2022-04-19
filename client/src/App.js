4965131cd608a80f772872bba94b3afbe30c95db
rohanarrancherry <rohanarrancherry@gmail.com>
2022-04-19 18:48:58 +0100

    editor role validation is done in editor pages

--- client/src/App.js
+++ client/src/App.js
@@ -6,27 +6,35 @@ import Editor from "./components/Editor";
 import EditorUi from "./components/Editor/editor";
 import Profile from "./components/Editor/profile";
 import React from "react";
+import axios from "axios";
+import data from "bootstrap/js/src/dom/data";
 
 
 function App() {
-  const user = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
+
+    const url = "/api/auth/user/role";
+    const user = fetch(url, {
+        headers: {Authorization: 'Bearer: ' + token}
+    }).then(res => res.json()).then(data => {
+        if (data.role === "user"){
+            return true
+        }
+        else if (data.role === "editor"){
+            return true
+        }
+        return false
+    });
 
   return (
       <Routes>
-        {/*{user && <Route path="/" exact element={<Main />} />}*/}
           {user && <Route path="/dashboard/:categoryParam" element={<Main/>} />}
 
           {user &&  <Route path="/editor/dashboard" exact element={<EditorUi />} />}
           {user && <Route path="/editor/profile" element={<Profile/>}/>}
-        <Route path="/signup" exact element={<Signup />} />
-        <Route path="/login" exact element={<Login />} />
-        
-
-          <Route path="/editor/dashboard" element={<EditorUi />} />
-          <Route path="/editor/profile" element={<Profile />} />
-        <Route path="/" element={<Navigate replace to="/login" />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/login" exact element={<Login />} />
           <Route path="/*" element={<Navigate replace to="/login" />} />
-          {/*<Route path="/editor" element={<Navigate replace to="/editor" />} />*/}
 
 
       </Routes>
