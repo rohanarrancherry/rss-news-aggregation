40411b1b6bd3933e9a81ca402efdcddd95fe6ab5
rohanarrancherry <rohanarrancherry@gmail.com>
2022-04-20 07:56:40 +0100

    feedsources favicon change

--- sources/feedSources.js
+++ sources/feedSources.js
@@ -4,84 +4,84 @@ const feedSources = [
         "source": "The Journal",
         "home": "https://www.breakingnews.ie",
         "category": "latest",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://www.irishtimes.com/cmlink/news-1.1319192",
         "source": "Irish News",
         "home": "https://www.breakingnews.ie",
         "category": "latest",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://www.thesun.ie/feed/",
         "source": "The Sun",
         "home": "https://www.breakingnews.ie",
         "category": "latest",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://abcnews.go.com/abcnews/usheadlines",
         "source": "ABC News",
         "home": "https://www.breakingnews.ie",
         "category": "latest",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://www.thetaste.ie/feed/",
         "source": "The Taste",
         "home": "https://www.breakingnews.ie",
         "category": "Food",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "http://www.techcentral.ie/feed/",
         "source": "Tech Central",
         "home": "https://www.breakingnews.ie",
         "category": "Technology",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml",
         "source": "NY Times",
         "home": "https://www.breakingnews.ie",
         "category": "latest",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://www.huffpost.com/section/front-page/feed?x=1",
         "source": "Huff Post",
         "home": "https://www.breakingnews.ie",
         "category": "latest",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://www.reddit.com/.rss",
         "source": "Reddit",
         "home": "https://www.breakingnews.ie",
         "category": "latest",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://feeds.bbci.co.uk/news/health/rss.xml",
         "source": "BBC UK",
         "home": "https://www.breakingnews.ie",
         "category": "health",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://www.independent.ie/sport/rss/",
         "source": "Independent",
         "home": "https://www.breakingnews.ie",
         "category": "sports",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       },
       {
         "url": "https://www.independent.ie/breaking-news/irish-news/?service=Rss",
         "source": "Independent",
         "home": "https://www.breakingnews.ie",
         "category": "national",
-        "favicon": "/assets/valioliiga-favicon-192x192.png"
+        "favicon": "/"
       }
 ];
 module.exports = feedSources;
